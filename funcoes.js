d1ba2e77d554a995aee19546b274176bfe4d431d
felipe <felipesantiago1404@hotmail.com>
2021-03-02 22:53:15 -0300

    tratando erro

--- funcoes.js
+++ funcoes.js
@@ -14,6 +14,9 @@ document.getElementById('resultado').innerHTML =  nome;
 var a = document.getElementById('text1').value;
 var b = document.getElementById('text2').value;
 var c = document.getElementById('text3').value;
+if (c=""){
+c=0
+}
 var resultado = (parseInt(a)-parseInt(b)/2+parseInt(c)/2)+1;
 var nome = ("O IDH CALCULADO FOI DE: "+ resultado);
 document.getElementById('resultado').innerHTML =  nome;
